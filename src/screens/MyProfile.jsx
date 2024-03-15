cec1eaa6289e67afc0ae848ba88d9c85ac00742b
gioviral77 <fergiral777@gmail.com>
2024-03-14 23:01:37 -0300

    Location

--- src/screens/MyProfile.jsx
+++ src/screens/MyProfile.jsx
@@ -30,6 +30,12 @@ const MyProfile = ({ navigation }) => {
       >
         <Text style={styles.text}>Add profile picture</Text>
       </Pressable>
+      <Pressable
+        style={styles.button}
+        onPress={() => navigation.navigate("Location Selector")}
+      >
+        <Text style={styles.text}>My Addresses</Text>
+      </Pressable>
     </View>
   );
 };
